f86e8cbbbc43d1b8e629b982a1938413a8435a3f
harsh gupta <harshnative@gmail.com>
2020-04-24 11:46:38 +0530

    added backUp class

---
+++ main/packages/backUp_utility/backUp.py
@@ -0,0 +1,230 @@
+import os
+import shutil
+import distutils.dir_util
+import datetime
+
+class backUp():
+    """ startBackUp is the main function of this , only this function is usefull as it can drive can other function of this class itself
+        
+            This function excepts three arguments 
+
+            1. commandList - list of command that are passed by the user to perform the actions
+                
+                commands that can be passed - 
+                    -a                  ->  for generating the backup of all the things of all the users 
+                    -a and -c           ->  for generating the backup of all the things of the current user
+                    -a and -e           ->  for generating the backup of all the essential things of all the users , essential items include things within - desktop , videos , pictures , music , downloads
+                    -a and -e and -c    ->  for generating the backup of all the essential things of the current user only
+                    -d                  ->  this can be clubbed with above and is used to backup the folders of which the paths are send as list to additionalDirectoryList
+
+            2. pass the list containing the exact paths of the folders of which you want to generate the backup of
+
+            3. pass the path for the backUp folder location 
+            
+            at the end of the process a log file will be generated at the desktop"""
+
+
+    # constructor function
+    def __init__(self):
+        self.userName = None
+        self.pathToBackup = None
+        self.exceptionList = []
+        self.listOfDirectories = []
+
+
+    # function to set the path of backup location 
+    def setPathToBackup(self , pathPassed):
+        self.pathToBackup = str(pathPassed)
+
+
+    # function to get the userName of the current user
+    def getUserName(self):
+        temp = os.environ # generates a object with the property called USERNAME containing the info
+        self.userName = temp["USERNAME"]
+
+
+    # function for copying the all stuff inside the c:/user
+    def forCommand_A(self):
+        self.exceptionList.clear()
+        path = "C:/Users"
+        try:
+            shutil.copytree(path , self.pathToBackup , dirs_exist_ok=True)
+        except Exception as e:
+            self.exceptionList.append(str(e))
+
+
+    # function for copying the essentials of all the users inside "C:/"
+    def forCommand_A_E(self):
+        """ desktop , downloads , pictures , videos , music , documents will be copied , 
+        if the file is already their in backup then they will be replaced by the comming new files"""
+
+        # stuff to use
+        pathList = []
+        pathToCopy = []
+        self.exceptionList.clear()
+        folderList = ["Desktop" , "Downloads" , "Pictures" , "Videos" , "Music" , "Documents"]
+        
+        # getting the list of users or directory names under user 
+        allUserList = [dI for dI in os.listdir("C:/Users") if os.path.isdir(os.path.join("C:/Users",dI))]
+
+        # adding the user names to c:/users/ to generate the paths
+        for i in allUserList:
+            stringToAppend = "C:/Users"
+            stringToAppend = stringToAppend + "/" + str(i) + "/"
+            pathList.append(stringToAppend)
+
+        # adding the folderNames to be copied to the previously generated paths
+        for i in pathList:
+            for j in folderList:
+                stringToAppend = str(i) + str(j)
+                if(os.path.isdir(stringToAppend)):
+                    pathToCopy.append(stringToAppend)
+
+        # stuff to use
+        lengthToCopy = len(pathToCopy)
+        count = 1
+
+        # actual copying starts here
+        for i in pathToCopy:
+            print(f"on {count} out of {lengthToCopy}")
+            # making sure that the path to backup exsist
+            try:
+                os.makedirs(self.pathToBackup + "/" + i[9:], exist_ok = True)
+            except OSError:
+                pass
+            
+            # making the copy
+            try:
+                shutil.copytree(i , self.pathToBackup + "/" + i[9:], dirs_exist_ok=True)
+            except Exception as e:
+                self.exceptionList.append(str(e))     
+            count += 1
+
+
+    # function for write the error data to a log file
+    def logFileGenerator(self):
+        """log file is located at desktop"""
+
+        path = "C:/Users/" + self.userName + "/Desktop/logFileJarvis.txt"
+
+        with open(path , "a+") as fil:
+
+            #writing some info to log file
+            currentDT = datetime.datetime.now()
+            fil.write("\n\n")
+            toWrite = "Process Time - " + str(currentDT)
+            fil.write(toWrite)
+            fil.write("\n")
+
+            #writing some actaul errors 
+            for i in self.exceptionList:
+                fil.write("Error - ")
+                fil.write(i)
+                fil.write("\n")
+                   
+
+    #function for copying all the current user
+    def forCommand_A_C(self):
+        """copies all the things inside the current user folder"""
+
+        # setting up the source path for copy function
+        path = "C:/Users/" + self.userName
+
+        # setting up the destination path for the copy function
+        try:
+            os.makedirs(self.pathToBackup + "/" + self.userName)
+        except OSError:
+                pass
+        
+        # copying
+        try:
+            shutil.copytree(path , self.pathToBackup + "/" + self.userName + "/" , dirs_exist_ok=True)
+        except Exception as e:
+            self.exceptionList.append(str(e))
+
+
+    # function for copying all the essential only of the current user
+    def forCommand_A_C_E(self):
+        pathList = []
+
+        folderList = ["Desktop" , "Downloads" , "Pictures" , "Videos" , "Music" , "Documents"]
+
+        for i in folderList:
+            stringToAppend = "C:/Users/" + self.userName + "/"
+            stringToAppend = stringToAppend + i
+            pathList.append(stringToAppend)
+
+        # stuff to use
+        lengthToCopy = len(pathList)
+        count = 1
+
+        # actual copying starts here
+        for i in pathList:
+            print(f"on {count} out of {lengthToCopy}")
+            # making sure that the path to backup exsist
+            try:
+                os.makedirs(self.pathToBackup + "/" + i[9:], exist_ok = True)
+            except OSError:
+                pass
+            
+            # making the copy
+            try:
+                shutil.copytree(i , self.pathToBackup + "/" + i[9:], dirs_exist_ok=True)
+            except Exception as e:
+                self.exceptionList.append(str(e))     
+            count += 1
+
+
+    # function to get the list of additional directories
+    def getListOfDirectories(self , listPassed):
+        for i in listPassed:
+            if(os.path.isdir(i)):
+                self.listOfDirectories.append(i)
+
+
+    # function for copying certain more directories from listPassed
+    def forCopyListOfDirectories(self):
+
+        for i in self.listOfDirectories:
+
+            string = i[:1] + i[2:]
+
+            try:
+                os.makedirs(self.pathToBackup + "/additionalFiles/" + string, exist_ok = True)
+            except OSError:
+                pass
+            
+            # making the copy
+            try:
+                shutil.copytree(i ,self.pathToBackup + "/additionalFiles/" + string , dirs_exist_ok=True)
+            except Exception as e:
+                self.exceptionList.append(str(e))
+
+
+    # driver function of the class this is what you will be calling
+    def startBackUp(self, commandList , additionalDirectoryList , pathToBackup):
+        
+        self.getUserName()
+        self.setPathToBackup(pathToBackup)
+        self.getListOfDirectories(additionalDirectoryList)
+
+        if(("-a" in commandList) and ("-c" in commandList) and ("-e" in commandList)):
+           self.forCommand_A_C_E()
+        elif(("-a" in commandList) and ("-c" in commandList) and ("-e" not in commandList)):
+            self.forCommand_A_C()
+        elif(("-a" in commandList) and ("-c" not in commandList) and ("-e" in commandList)): 
+            self.forCommand_A_E()
+        elif(("-a" in commandList) and ("-c" not in commandList) and ("-e" not in commandList)):
+            self.forCommand_A()
+        
+        if("-d" in commandList):
+            self.forCopyListOfDirectories()
+
+        self.logFileGenerator()
+
+
+# driver code for testing purpose only
+if __name__ == "__main__":
+    obj = backUp()
+    commandList = ["-d"]
+    obj.startBackUp(commandList , ["C:/Users/harsh/Desktop/MY files/images"] , "F:/hello")
--- main/packages/weather/getWeather.py
+++ main/packages/weather/getWeather.py
@@ -3,68 +3,113 @@ import json
 
 """ this is using the open weather api """
 
-def makeUrl(apiId , cityName):
-    stringKey = "https://api.openweathermap.org/data/2.5/weather?q="
-    stringKey = stringKey + str(cityName) + "&appid=" + str(apiId)
-    return stringKey
+class weatherData():
+
+    # building up constructor to set the defualt value's
+    def __init__(self):
+        self._apiKey = "fe82651e607e46db61dba45e39aa7e17"
+        self.cityName = "london"
+        self.stringKey = "https://api.openweathermap.org/data/2.5/weather?q="
+        self.data = None
+        self.jsonData = None
+        self.resultList = []
+        self.listPass = []
+        self.tempInK = 273
+        self.tempInC = 0
+        self.tempInF = 0
 
 
-def getDataFromWeb(url):
-    try:
-        data = requests.get(url).text
-        return data
-    except Exception:
-        return False
+    # function to set the cityName for which the weather data will be pulled
+    def giveCityName(self , cityNamePassed):
+        self.cityName = str(cityNamePassed)
 
 
-def loadDataIntoJson(data):
-    if data == False:
-        return False
-    else:
-        j = json.loads(data)
-        return j
+    # function to make the final url which will be used to make request
+    def makeUrl(self):  
+        self.stringKey = self.stringKey + self.cityName + "&appid=" + self._apiKey
 
 
-def extractInfo(listPass , jsonData):
-    """ the list is passed containing exact name of which the information will be returned 
-        , if the listElement is not in the Json file Then a False Bool value is added to list in its place"""
+    # function to get the weather data from the open weather server
+    def getDataFromWeb(self):
+        try:
+            self.data = requests.get(self.stringKey).text
+        except Exception:
+            pass    # since the defualt value of the data is already None
 
-    """ these are the elements that can be found 
-    temp	:	280.32
-    pressure	:	1012
-    humidity	:	81
-    temp_min	:	279.15
-    temp_max	:	281.15
-    visibility	:	10000"""
 
-    resultList = []
-
-    for i in listPass:
-        if(jsonData == False):
-            resultList.append(False)
+    # function to convert the data from the web to json format
+    def loadDataIntoJson(self):
+        if self.data == None:
+            pass
         else:
-            try:
-                resultList.append(jsonData["main"][str(i)])
-            except KeyError:
-                resultList.append(False)
+            self.jsonData = json.loads(self.data)
 
-    return resultList
-        
+
+    # function to read the list of infomation need and append the result to resultList by getting the values from the json
+    # read the function docs for getting the info on listPassing
+    def extractInfo(self):
+        """ the list is passed containing exact name of which the information will be returned 
+            , if the listElement is not in the Json file Then a None value is added to list in its place"""
+
+        """ these are the elements that can be found 
+        temp	:	280.32
+        pressure	:	1012
+        humidity	:	81
+        temp_min	:	279.15
+        temp_max	:	281.15
+        visibility	:	10000
+        tempInC : get the temp in celcius
+        tempInF : get the temp in faraniet """
+
+        for i in self.listPassed:
+            if(self.jsonData == None):
+                self.resultList.append(None)
+            elif(i == "tempInC"):
+                self.resultList.append(self.tempInC)
+            elif(i == "tempInF"):
+                self.resultList.append(self.tempInF)
+            else:
+                try:
+                    self.resultList.append(self.jsonData["main"][str(i)])
+                except KeyError:
+                    self.resultList.append(None)
+
+    
+    # function to convert the json temp which is in kelvin to 'c
+    def convTempToC(self):
+        self.tempInK = self.jsonData["main"]["temp"]
+        self.tempInC = self.tempInK - 273
+
+
+    # function to convert the 'C to 'F
+    def convTempToF(self):
+        self.tempInF = ( self.tempInC * (9/5) ) + 32
+
+
+    # function that the user will be using to get the data
+    # function to do all the above stuff line wise 
+    def getWeatherData(self , give_cityName , give_listOfValueToGet):
+        """Read the extractInfo() docs to get info on give_listOfValueToGet and the info on the things that are returned in list"""
+        self.listPassed = give_listOfValueToGet
+        self.giveCityName(give_cityName)
+        self.makeUrl()
+        self.getDataFromWeb()
+        self.loadDataIntoJson()
+        self.convTempToC()
+        self.convTempToF()
+        self.extractInfo()
+        return self.resultList
+   
 
 # driver code for test run
 if __name__ == "__main__":
-    url = makeUrl("fe82651e607e46db61dba45e39aa7e17", "london")
-
-    apiData = getDataFromWeb(url)
-
-    jData = loadDataIntoJson(apiData)
-
-    myList = [ "temp" ,
+    
+    obj = weatherData()
+    myList = [ "tempInC" ,
                "pressure" , 
                "humidity" , "hello" ]
 
-    result = extractInfo(myList , jData)
+    print(obj.getWeatherData("delhi" , myList))
 
-    print(result)
 
 
